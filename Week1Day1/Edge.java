13980715c8b3a08915cadcfd26f4e1bf01d16a41
cvaskp <cvaskp@gmail.com>
2025-06-22 13:14:59 +0530

    Add files via upload

---
+++ Week1Day1/Edge.java
@@ -0,0 +1,16 @@
+package Week1Day1;
+
+public class Edge {
+
+	public static void main(String[] args) {
+		String developer = "Microsoft";
+		float version = 137.0f;
+		int releaseYear = 2015;
+		boolean isBeta = false;
+		char shortcutKey = 'E';
+		System.out.println("Browser Version: "+version+"\n"+"Developer: "+developer+"\n"+"Release Year: "+releaseYear+"\n"+"IsBeta: "+isBeta+"\n"+"Shortcut Key for browser is: "+shortcutKey);
+	
+
+	}
+
+}
